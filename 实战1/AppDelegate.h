0e157e638b3173920da3a69c522d28d37363126e
apple <apple@mayingbing.local>
2015-10-29 20:53:12 +0800

    Initial Commit

---
+++ 实战1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  实战1
+//
+//  Created by apple on 15/10/29.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 实战1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  实战1
+//
+//  Created by apple on 15/10/29.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
